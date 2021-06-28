8fc8c9382ffd98e9af5671cd945d3fb14f7bee05
Abdul Dayur <Abdul.dayur@nike.com>
2021-06-28 10:50:35 -0700

    added struct for photo user

---
+++ SnappyGram/Models/PhotoUser.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoUser.swift
+//  SnappyGram
+//
+//  Created by Abdul Dayur on 6/28/21.
+//
+
+import Foundation
+
+struct PhotoUser {
+    var userID: String?
+    var userName: String?
+    
+}
