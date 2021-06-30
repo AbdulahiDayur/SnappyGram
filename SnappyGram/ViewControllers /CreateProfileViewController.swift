5da8dee7c6f2de55cdcd8f9db08f26334bdcaab3
Abdul Dayur <Abdul.dayur@nike.com>
2021-06-30 12:08:06 -0700

    added func that checks if user created profile

--- SnappyGram/ViewControllers /CreateProfileViewController.swift
+++ SnappyGram/ViewControllers /CreateProfileViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class CreateProfileViewController: UIViewController {
     
@@ -19,6 +20,33 @@ class CreateProfileViewController: UIViewController {
     }
     
     @IBAction func confimTapped(_ sender: Any) {
+        
+        // Check if a user is logged in
+        guard Auth.auth().currentUser != nil else {
+            return
+        }
+        
+        // Get username  and check against whitespaces
+        let username = usernameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if username == nil || username == "" {
+            return
+        }
+        
+        // create profile
+        UserService.createProfile(userId: Auth.auth().currentUser!.uid, username: username!) { (user) in
+            
+            if user != nil {
+                let tabBarVC = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.tabBarController)
+                
+                self.view.window?.rootViewController = tabBarVC
+                self.view.window?.makeKeyAndVisible()
+                
+            } else {
+                
+            }
+            
+        }
         
     }
     
