f2137cbff8095b36e2953f7ec4f40d6bdf1e04b2
Abdul Dayur <Abdul.dayur@nike.com>
2021-06-30 11:36:52 -0700

    customized reusable varibals

---
+++ SnappyGram/Helpers/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  SnappyGram
+//
+//  Created by Abdul Dayur on 6/28/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let tabBarController = "mainTabBarController"
+        static let profileSegue = "goToCreateProfile"
+    }
+}
