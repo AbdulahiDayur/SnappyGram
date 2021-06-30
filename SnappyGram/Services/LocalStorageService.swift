0350a1dc158a2cb610a7dcd1c48b2ec75e27fbc9
Abdul Dayur <Abdul.dayur@nike.com>
2021-06-30 13:58:47 -0700

    Created local storage file to save user data

---
+++ SnappyGram/Services/LocalStorageService.swift
@@ -0,0 +1,21 @@
+//
+//  LocalStorageService.swift
+//  SnappyGram
+//
+//  Created by Abdul Dayur on 6/30/21.
+//
+
+import Foundation
+
+class LocalStorageService {
+    
+    static func saveUser(userId: String, username: String) {
+        
+        // Get a reference to user defaults
+        let defaults = UserDefaults.standard
+        
+        // Save the userId and username to defaults
+        defaults.set(userId, forKey: "storedId")
+        defaults.set(username, forKey: "storedUsername")
+    }
+}
