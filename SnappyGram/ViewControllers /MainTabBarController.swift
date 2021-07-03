3b498b0eb557212fb90d869500a3719d2ced6628
Abdul Dayur <Abdul.dayur@nike.com>
2021-07-02 18:16:27 -0700

    added action sheet for when the "Add" tabBar is tapped

--- SnappyGram/ViewControllers /MainTabBarController.swift
+++ SnappyGram/ViewControllers /MainTabBarController.swift
@@ -15,7 +15,45 @@ class MainTabBarController: UITabBarController {
         // Do any additional setup after loading the view.
     }
     
-di
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        
+        // Detect which tab was tapped on
+        if item.tag == 2 {
+            
+            // Camera tab was tapped on
+            
+            // Create the action sheet
+            let actionSheet = UIAlertController(title: "Add a Photo", message: "Select a source", preferredStyle: .actionSheet)
+            
+            // only add camera button it's available
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                
+                // Create and add the Camera button
+                let cameraButton = UIAlertAction(title: "Camera", style: .default) { (action) in
+                    
+                    // Display the UiimagePickerController set to camera mode
+                }
+                actionSheet.addAction(cameraButton)
+            }
+            
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                
+                // Create and the Photo Library button
+                let libraryButton = UIAlertAction(title: "Photo Library", style: .default) { (action) in
+                    
+                    // Display the UIimagePickercontroller set to library mode
+                }
+                actionSheet.addAction(libraryButton)
+            }
+            
+            // Create and add the Cancel button
+            let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            actionSheet.addAction(cancelButton)
+            
+            // Display the action sheet
+            present(actionSheet, animated: true, completion: nil)
+        }
+    }
 
 
 }
