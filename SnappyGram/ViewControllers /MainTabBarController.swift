f9137b02c2bf42967169dca33a5dd9ed4c810d9b
Abdul Dayur <Abdul.dayur@nike.com>
2021-07-03 12:14:02 -0700

    Display the UiimagePickerController set to proper mode

--- SnappyGram/ViewControllers /MainTabBarController.swift
+++ SnappyGram/ViewControllers /MainTabBarController.swift
@@ -25,13 +25,14 @@ class MainTabBarController: UITabBarController {
             // Create the action sheet
             let actionSheet = UIAlertController(title: "Add a Photo", message: "Select a source", preferredStyle: .actionSheet)
             
-            // only add camera button it's available
+            // only add camera button if it's available
             if UIImagePickerController.isSourceTypeAvailable(.camera) {
                 
                 // Create and add the Camera button
                 let cameraButton = UIAlertAction(title: "Camera", style: .default) { (action) in
                     
                     // Display the UiimagePickerController set to camera mode
+                    self.showImagePickerController(mode: .camera)
                 }
                 actionSheet.addAction(cameraButton)
             }
@@ -42,6 +43,7 @@ class MainTabBarController: UITabBarController {
                 let libraryButton = UIAlertAction(title: "Photo Library", style: .default) { (action) in
                     
                     // Display the UIimagePickercontroller set to library mode
+                    self.showImagePickerController(mode: .photoLibrary)
                 }
                 actionSheet.addAction(libraryButton)
             }
@@ -54,6 +56,32 @@ class MainTabBarController: UITabBarController {
             present(actionSheet, animated: true, completion: nil)
         }
     }
+    
+    func showImagePickerController(mode: UIImagePickerController.SourceType) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = mode
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
 
-
+extension MainTabBarController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        // Dismiss the image picker
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // Get reference to selected image
+        let selectedImage = info[.originalImage] as? UIImage
+        
+        if let selectedImage = selectedImage {
+            
+        }
+    }
 }
