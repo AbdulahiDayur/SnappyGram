3347cd1324b09096d4458ffc081a5bd6cfcad475
Abdul Dayur <Abdul.dayur@nike.com>
2021-07-04 14:26:59 -0700

    Created new photo struct for adding to feed

---
+++ SnappyGram/Models/Photo.swift
@@ -0,0 +1,18 @@
+//
+//  Photo.swift
+//  SnappyGram
+//
+//  Created by Abdul Dayur on 7/3/21.
+//
+
+import Foundation
+
+struct Photo {
+    
+    var photoId: String?
+    var byId: String?
+    var byUsername: String?
+    var date: String?
+    var url: String?
+    
+}
