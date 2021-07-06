142b477027ecdcd8dde6189d3151bd87de8ef236
Abdul Dayur <Abdul.dayur@nike.com>
2021-07-06 16:51:41 -0700

    configuring code for progress bar details

--- SnappyGram/ViewControllers /CameraViewController.swift
+++ SnappyGram/ViewControllers /CameraViewController.swift
@@ -8,17 +8,37 @@
 import UIKit
 
 class CameraViewController: UIViewController {
-
+    
+    @IBOutlet var progressLabel: UILabel!
+    
+    @IBOutlet var progressBar: UIProgressView!
+    
+    @IBOutlet var doneButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // Hide and reset all elements
+        progressBar.alpha = 0
+        progressBar.progress = 0
+        
+        doneButton.alpha = 0
+        progressLabel.alpha = 0
+        
+        
+    }
+    
     func savePhoto(image: UIImage) {
         
         // Photo Service stores the photo
-        PhotoService.savePhoto(image: image)
+        PhotoService.savePhoto(image: image) { (pct) in
+            
+        }
     }
 
 }
