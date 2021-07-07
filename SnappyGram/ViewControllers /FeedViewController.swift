70a1b0af024ba568598437951041d421b0cea274
Abdul Dayur <Abdul.dayur@nike.com>
2021-07-07 13:10:00 -0700

    added tableview delegate stub methods

--- SnappyGram/ViewControllers /FeedViewController.swift
+++ SnappyGram/ViewControllers /FeedViewController.swift
@@ -16,13 +16,35 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         PhotoService.retrievePhotos { (retrievedPhotos) in
             
             self.photos = retrievedPhotos
-            
             self.tableView.reloadData()
         }
     }
-    
-
 }
+
+extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.Storyboard.photoCellId, for: indexPath) as? PhotoCell
+        
+        let photo = self.photos[indexPath.row]
+        
+        cell?.displayPhoto(photo: photo)
+        
+        return cell!
+    }
+    
+    
+    
+}
+
+
