ec8b322bfa23b76906f3182de27b2819aa6a6242
Abdul Dayur <Abdul.dayur@nike.com>
2021-07-06 22:35:53 -0700

    initialized some struct variables

--- SnappyGram/Models/Photo.swift
+++ SnappyGram/Models/Photo.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import FirebaseFirestore
 
 struct Photo {
     
@@ -14,5 +15,21 @@ struct Photo {
     var byUsername: String?
     var date: String?
     var url: String?
+    
+    init?(snapshot: QueryDocumentSnapshot) {
+        
+        // Parse the data out
+        let data = snapshot.data()
+        
+        let photoId = data["photoId"] as? String
+        let userId = data["byId"] as? String
+        let username = data["byUsername"] as? String
+        let date = data["date"] as? String
+        let url = data["url"] as? String
+        
+        if photoId == nil || userId == nil || username == nil || date == nil || url == nil {
+            return nil
+        }
+    }
     
 }
